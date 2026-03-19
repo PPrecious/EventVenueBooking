294dbd27ac83df4c0a877f5b8ac32a6d98c6fa1c
PPrecious <144118997+PPrecious@users.noreply.github.com>
2026-03-19 20:36:17 +0200

    Add files via upload

---
+++ dbo.Events.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Events] (
+    [EventId]          INT           NOT NULL,
+    [EventName]        VARCHAR (150) NOT NULL,
+    [EventDate]        DATE          NOT NULL,
+    [EventDescription] VARCHAR (500) NULL,
+    [VenueId]          INT           NULL,
+    PRIMARY KEY CLUSTERED ([EventId] ASC),
+    CONSTRAINT [FK_Events_Venues] FOREIGN KEY ([VenueId]) REFERENCES [dbo].[Venues] ([VenueId])
+);
+
